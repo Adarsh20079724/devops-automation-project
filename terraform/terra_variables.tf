354bc2fe47a25cecbffe5bdecf58bac5d3f690fc
Adarsh6818 <adarshvish01@gmail.com>
2025-10-26 18:57:37 +0000

    terraform variables created

---
+++ terraform/terra_variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "devops-app"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ssh_key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your IP address for SSH access"
+  type        = string
+}
