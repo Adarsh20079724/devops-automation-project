9f7d1b80342b05e86e8e8cf2938584c75a0ce8f0
Adarsh6818 <adarshvish01@gmail.com>
2025-10-26 21:36:51 +0000

    Created output, security, vpc and ec2 terraform files

---
+++ terraform/terra_security_groups.tf
@@ -0,0 +1,46 @@
+# Security Group for EC2
+resource "aws_security_group" "app_server" {
+  name        = "${var.project_name}-app-sg"
+  description = "Security group for application server"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH access
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  # HTTP access
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Application port
+  ingress {
+    description = "App port from anywhere"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound traffic
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-sg"
+  }
+}
