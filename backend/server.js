a65dacd0f32bb99d13187da4f7ac0e97741b3eb4
Adarsh6818 <adarshvish01@gmail.com>
2025-11-06 02:14:17 +0000

    backend fixed. test deployment 4

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
