62c88d1eb91c4625d8ce6ece97c728bb91acd1fe
Adarsh6818 <adarshvish01@gmail.com>
2025-11-09 02:09:40 +0000

    Minor changes. Final test Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,21 @@ const App = () => {
 
   const [count, setCount] = useState(0)
 
-
   return (
     <div className="container">
-      <h1>🚀 Hello World from DevOps!</h1>
+      <h1>Deployment Successfull!</h1>
       <p>This React app is deployed using:</p>
       <ul>
-        <li>✅ Docker</li>
-        <li>✅ Nginx</li>
-        <li>✅ Terraform</li>
-        <li>✅ Ansible</li>
-        <li>✅ GitHub Actions</li>
-        <li>✅ AWS EC2</li>
-        <li>Just now EC2 was Provisioned. Test Change</li>
+        <li>Docker</li>
+        <li>Nginx</li>
+        <li>Terraform</li>
+        <li>Ansible</li>
+        <li>GitHub Actions</li>
+        <li>AWS EC2</li>
+  
         <button onClick={() => setCount(count + 1)}>{`Count ${count}`}</button>
       </ul>
-      <p className="success">🎉 Deployment Successful!</p>
+      <p className="success">Deployment Successfull!</p>
     </div>
   )
 }
